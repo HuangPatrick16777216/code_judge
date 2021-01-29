3f800425673f49480964e3a82f6f4e457b6721c5
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-01-28 19:14:11 -0800

    Client class.

--- version2/server.py
+++ version2/server.py
@@ -15,7 +15,7 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
-#! WARNING:
+#! ##### WARNING: #####
 #! The server judge will execute files submitted by users.
 #! If those files contain malicious code, your system may be messed up.
 #! At the time of this writing, there is nothing to prevent this.
@@ -40,12 +40,40 @@ class Server:
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((ip, port))
 
+        self.clients = []
+
     def start(self):
         self.server.listen()
         print(f"{Fore.GREEN}Server started on {self.ip}{Fore.RESET}")
 
         while True:
             conn, addr = self.server.accept()
+            client = Client(conn, addr)
+            self.clients.append(client)
+
+
+class Client:
+    def __init__(self, conn, addr):
+        self.conn = conn
+        self.addr = addr
+
+        self.alert("INFO", "Connected")
+        self.auth()
+        threading.Thread(target=self.start).start()
+
+    def start(self):
+        pass
+
+    def alert(self, type, msg):
+        color = Fore.RESET
+        if type == "INFO":
+            color = Fore.CYAN
+        elif type == "WARNING":
+            color = Fore.YELLOW
+        elif type == "ERROR":
+            color = Fore.RED
+
+        print(f"{color}[{self.addr}] {msg}{Fore.RESET}")
 
 
 def main():
