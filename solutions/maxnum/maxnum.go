0e779519aa0450a9e1740e1c34ec80133c973fa5
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-30 15:03:03 -0800

    finished max num solution

---
+++ solutions/maxnum/maxnum.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n, curr int
+	fmt.Scanf("%d", &n)
+	max := -1
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &curr)
+		if curr > max {
+			curr = n
+		}
+	}
+	fmt.Println(curr)
+}
