68f7e9ac7b969c16c2311e282b1b090f3a180683
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-30 15:04:36 -0800

    improved addone.go

--- solutions/addone/addone.go
+++ solutions/addone/addone.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = strings.Replace(input, "\n", "", -1)
-	num, _ := strconv.Atoi(input)
-	fmt.Println(strconv.Itoa(num+1))
+	var n int
+	fmt.Scanf("%d", &n)
+	fmt.Println(n+1)
 }
