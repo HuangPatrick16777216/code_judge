af92fd7c6900ba88a08f103478cadc62e066527f
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-01-30 21:46:57 -0800

    Java addone solution.

---
+++ solutions/addone/addone.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str_n = scan.nextLine();
+        int n = Integer.parseInt(str_n);
+        System.out.println(n+1);
+    }
+}
