98d8247ae7303020a7e2e24226fcb3c606859a47
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-01-31 18:32:45 -0800

    C++ solution for binsearch.

---
+++ solutions/binsearch/binsearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int search(std::vector<int> nums, int q, int start, int end) {
+    if (end-start <= 2) {
+        if (nums[start] == q) return start;
+        else return start+1;
+    }
+
+    int mid = (end+start) / 2;
+    if (nums[mid] > q) return search(nums, q, start, mid);
+    else return search(nums, q, mid, end);
+}
+
+int main() {
+    std::vector<int> nums, queries;
+    int len, num_q;
+
+    std::cin >> len >> num_q;
+    for (auto i = 0; i < len; i++) {
+        int tmp;
+        std::cin >> tmp;
+        nums.push_back(tmp);
+    }
+    for (auto i = 0; i < num_q; i++) {
+        int tmp;
+        std::cin >> tmp;
+        queries.push_back(tmp);
+    }
+
+    for (auto q: queries) {
+        std::cout << search(nums, q, 0, len)+1 << std::endl;
+    }
+}
